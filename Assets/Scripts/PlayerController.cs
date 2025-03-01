0a0a3b3005ab90e21b8afe05abf05fe9fd7b6759
Viktor Jarl Palmason <viktorjarlpalmason@gmail.com>
2025-03-01 20:12:20 +0100

    Refactored the player movement to improve the controlls.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float maxSpeed = 10f;
+    [Range(1, 2)]
+    [SerializeField] float deAcceleration = 1.2f;
 
     Rigidbody2D body;
     InputAction move;
+
+    // The direction the players across the x axis
+    float xDir = Mathf.Sin(Mathf.Deg2Rad * 105f);
+    float yDir = Mathf.Sin(Mathf.Deg2Rad * 150f);
+
+    //Vector2 moveDir = new Vector2(Mathf.Sin(Mathf.Deg2Rad * 105f), Mathf.Sin(Mathf.Deg2Rad * 150f));
+    Vector2 currentDir;
+
+    Vector2 NorthEast;
+    Vector2 NorthWest;
+    Vector2 SouthWest;
+    Vector2 SouthEast;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         move = InputSystem.actions.FindAction("Player/Move");
+
+        NorthEast = new Vector2(xDir, yDir).normalized;
+        NorthWest = new Vector2(-xDir, yDir).normalized;
+        SouthWest = new Vector2(-xDir, -yDir).normalized;
+        SouthEast = new Vector2(xDir, -yDir).normalized;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,6 +53,28 @@ public class PlayerController : MonoBehaviour
     void ProcessMovement()
     {
         Vector2 input = move.ReadValue<Vector2>();
-        body.AddForce(input * movementSpeed * Time.fixedDeltaTime);       
+
+        if(Keyboard.current.wKey.isPressed)
+        {
+            body.linearVelocity = NorthEast * maxSpeed;
+        } else if(Keyboard.current.aKey.isPressed)
+        {
+            body.linearVelocity = NorthWest * maxSpeed;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            body.linearVelocity = SouthWest * maxSpeed;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            body.linearVelocity = SouthEast * maxSpeed;
+        }
+        {
+            if(body.linearVelocity.magnitude < 1f)
+            {
+                body.linearVelocity = Vector2.zero;
+            }
+            body.linearVelocity /= deAcceleration;
+        }
     }
 }
