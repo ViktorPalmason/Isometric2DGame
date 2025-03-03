41a09bd81673b9f87f3cbab8d6c89aa9b78777f0
Viktor Jarl Palmason <viktorjarlpalmason@gmail.com>
2025-03-03 20:24:33 +0100

    A projectile is instantiated when the left mouse button is clicked.

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,25 +4,38 @@ using UnityEngine.InputSystem;
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    Mouse mouse = Mouse.current;
+    [SerializeField] GameObject projectile;
 
+    Mouse mouse = Mouse.current;
+    InputAction attack;
     private void Awake()
     {
         if (cam == null)
         {
             cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         }
+
+        attack = InputSystem.actions.FindAction("Player/Attack");
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mouseCord = cam.ScreenToWorldPoint(mouse.position.ReadValue());
-        Debug.Log(mouseCord);
+        if(attack.WasPressedThisFrame())
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //Vector3 mouseCord = cam.ScreenToWorldPoint(mouse.position.ReadValue());
+        //Debug.Log(mouseCord);
+        Instantiate(projectile, transform.position + Vector3.right, Quaternion.identity);
     }
 }
