f2cd8468046d4c3f3fbf20ce00436b5237e670a5
Viktor Jarl Palmason <viktorjarlpalmason@gmail.com>
2025-03-03 22:42:46 +0100

    Gave the projectile velociy and a distance that it travels before getting destroyed.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,15 +3,32 @@ using UnityEngine.InputSystem;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] float speed = 20f;
+    [SerializeField] Camera mainCam;
+    [SerializeField] float distance = 10f;
+    
+    Rigidbody2D body;
+
+    Vector3 startPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        body = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        Vector3 mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        //Vector3 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 dir = mousePos - transform.position;
+        body.linearVelocity = new Vector2(dir.x, dir.y).normalized * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Vector3.Distance(startPos, transform.position) > distance)
+        {
+            Destroy(gameObject);
+        }
     }
 }
